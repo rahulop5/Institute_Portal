1c4772e782f6956f946ee6932cce1a92aa3483ac
Rahul <venkatrahulxyz@gmail.com>
2025-07-09 19:17:05 +0530

    frontend template added

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import env from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import ugstudentbtpRoutes from "./routes/ugstudentbtpRoutes.js";
 import staffbtpRoutes from "./routes/staffbtpRoutes.js";
 import facultybtpRoutes from "./routes/facultybtpRoutes.js";
 
 const app=express();
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 env.config();
@@ -16,5 +20,11 @@ app.use("/auth", authRoutes);
 app.use("/student/btp", ugstudentbtpRoutes);
 app.use("/staff/btp", staffbtpRoutes);
 app.use("/faculty/btp", facultybtpRoutes);
+
+app.get("/test", (req, res)=>{
+    res.json({
+        message: "Hello from venakamaala(backend)"
+    });
+})
 
 app.listen(3000);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  //this is how u fetch the data from backend
+  const [tempdata, settempdata]=useState("YO")
+  useEffect(()=>{
+    const testfn=async ()=>{
+      const response=await fetch("http://localhost:3000/test");
+      const propres=await response.json();
+      settempdata(propres.message);
+    }
+    testfn();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p className='temp'>{tempdata}</p>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
